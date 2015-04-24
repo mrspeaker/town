58d91931f728ccf3288839b157a7a5c4af97400f
Mr Speaker <mrspeaker@gmail.com>
2015-04-24 12:45:33 -0400

    fix resize

--- src/House.js
+++ src/House.js
@@ -6,17 +6,17 @@ things.load.then(() => {
 
   house = new THREE.Object3D();
 
-  let wall = things.o.wall.clone();
+  const wall = things.o.wall.clone();
   wall.position.set(0, 0, 0);
   wall.rotation.set(0, 0, 0);
   house.add(wall);
 
-  let wall2 = things.o.wall.clone();
+  const wall2 = things.o.wall.clone();
   wall2.position.set(2.5, 0, 0);
   wall2.rotation.set(0, 0, 0);
   house.add(wall2);
 
-  let roof = things.o.roof.clone();
+  const roof = things.o.roof.clone();
   roof.position.set(1.45, 0.85, -1.65);
   roof.rotation.set(0, 0, 0);
   house.add(roof);
--- src/env.js
+++ src/env.js
@@ -1,17 +1,16 @@
-let h = window.innerHeight;
-let w = window.innerWidth;
-const handlers = [];
+const env = {
+  w: window.innerWidth,
+  h: window.innerHeight,
+  onResize(handler) {
+    handlers.push(handler);
+  }
+}
 
+const handlers = [];
 window.addEventListener('resize', () => {
   env.w = window.innerWidth;
   env.h = window.innerHeight;
   handlers.forEach(h => h(env));
 }, false);
 
-export default {
-  w,
-  h,
-  onResize(handler) {
-    handlers.push(handler);
-  }
-}
+export default env;
